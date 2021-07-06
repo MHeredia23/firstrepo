95f970ed39a8ab9efa02f2ca886baf08ec130c15
MHeredia23 <marcos.heredia@mercadolibre.com>
2021-07-06 14:57:27 -0300

    Ejercicio Calculadora

---
+++ CalculadoraMetros/src/main/java/com/example/calculadorametros/CalculadoraMetrosApplication.java
@@ -0,0 +1,13 @@
+package com.example.calculadorametros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraMetrosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculadoraMetrosApplication.class, args);
+    }
+
+}
---
+++ CalculadoraMetros/src/test/java/com/example/calculadorametros/CalculadoraMetrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calculadorametros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraMetrosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
