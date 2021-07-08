c8ff42f4eb7e857a08461b50d67eb67f6ff86e5f
MHeredia23 <marcos.heredia@mercadolibre.com>
2021-07-08 16:32:56 -0300

    FIx error

--- CalorieCalculator/src/main/java/com/example/caloriecalculator/exceptions/ExceptionControllerAdvice.java
+++ CalorieCalculator/src/main/java/com/example/caloriecalculator/exceptions/ExceptionControllerAdvice.java
@@ -1,6 +1,5 @@
 package com.example.caloriecalculator.exceptions;
 
-import com.example.caloriecalculator.dtos.response.ExceptionResponseDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -16,11 +15,11 @@ public class ExceptionControllerAdvice {
     @ExceptionHandler
     @ResponseBody
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ExceptionResponseDTO handlerException(MethodArgumentNotValidException e){
+    public com.example.linktracker.dtos.response.ExceptionResponseDTO handlerException(MethodArgumentNotValidException e){
         List<FieldError> fieldErrors = e.getBindingResult().getFieldErrors();
         Map<String, String> details = new HashMap<>();
         fieldErrors.forEach(error-> details.put(error.getField(),error.getDefaultMessage()));
-        return ExceptionResponseDTO.builder()
+        return com.example.linktracker.dtos.response.ExceptionResponseDTO.builder()
                 .status(HttpStatus.BAD_REQUEST.value())
                 .msg("Error en los datos de entrada")
                 .details(details)
